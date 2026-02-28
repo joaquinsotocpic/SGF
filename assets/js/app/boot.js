0c4cb5e94e9ef23409ba4761d7e467c7248c213e
joaquinsotocpic <joaquin.soto@cpic.cr>
2026-02-28 08:41:25 -0600

    Bump app version to 1.32.6

--- assets/js/app/boot.js
+++ assets/js/app/boot.js
@@ -1,7 +1,7 @@
 // Boot SGF v1.17.16
 
 window.SGF = window.SGF || {};
-window.SGF.APP_VERSION = '1.32.5';
+window.SGF.APP_VERSION = '1.32.6';
 
 // Nota: La delegación del Split de movimientos vive en el módulo de Movimientos.
 // Mantenerla aquí causaba doble-toggle (dos listeners capturando el mismo click),
